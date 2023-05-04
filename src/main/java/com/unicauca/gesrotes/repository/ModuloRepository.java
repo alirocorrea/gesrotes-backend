d25d4829dfbe524a81f01648414261cf3c4a9c47
jAlvarez <juliodesarrollo@hospitalsanjose.gov.co>
2023-05-04 10:41:56 -0500

    correccion consulta sql

--- src/main/java/com/unicauca/gesrotes/repository/ModuloRepository.java
+++ src/main/java/com/unicauca/gesrotes/repository/ModuloRepository.java
@@ -13,6 +13,6 @@ public interface ModuloRepository extends JpaRepository<Modulo, Long>{
     
     List<Modulo> findByNombre(String nombre);
 
-    @Query(nativeQuery = true, value = "SELECT DISTINCT\tm.* FROM Asignaturas AS a INNER JOIN AsignaturaDocentes AS ad ON a.id_Asignaturas = ad.Asignaturas_id_Asignaturas INNER JOIN Docentes AS d ON ad.Docentes_id_docente = d.id_docente INNER JOIN Modulos AS m ON d.id_docente = m.fk_id_DocentesMod LEFT JOIN HorariosModulos AS h ON m.id_Modulos = h.fk_id_Modulos WHERE d.id_docente = ?1 AND a.id_Asignaturas = ?2 AND h.fk_id_Modulos IS NULL ")
+    @Query(nativeQuery = true, value = "SELECT DISTINCT m.* FROM Asignaturas AS a INNER JOIN AsignaturaDocentes AS ad ON a.id_Asignaturas = ad.Asignaturas_id_Asignaturas INNER JOIN Docentes AS d ON ad.Docentes_id_docente = d.id_docente INNER JOIN Modulos AS m ON d.id_docente = m.fk_id_DocentesMod LEFT JOIN HorariosModulos AS h ON m.id_Modulos = h.fk_id_Modulos WHERE d.id_docente = ?1 AND a.id_Asignaturas = ?2 AND h.fk_id_Modulos IS NULL ")
     List<Modulo> listaModulosSinHorarioAsignado(Long id_docente, Long id_Asignatura);
 }
